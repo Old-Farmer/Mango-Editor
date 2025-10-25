c6ba104ba34e2227097b2ac6e0f32f33500f2524
charlie <chaishixin2000@qq.com>
2025-10-26 02:52:11 +0800

    Revert "feat: rename help.txt to help.md"
    This reverts commit 1b2477ef56c9bf71d046e03419a28bbd5bcd6cda.

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
 constexpr const char* kDocsPath = "docs";
-constexpr const char* kHelpPath = "docs/help.md";
+constexpr const char* kHelpPath = "docs/help.txt";
 
 constexpr const char* kKeywordStr = "keyword";
 constexpr const char* kStringStr = "string";
